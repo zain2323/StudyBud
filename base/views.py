6bffce485642c570f01b2ab7fb90ae82e75161ee
Zain Siddiqui <zainsiddiqui2323@gmail.com>
2023-02-15 16:06:28 +0500

    Initial commit (Django StudyBud). Added basic templates and CRUD functionality

---
+++ base/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
+# Create your views here.
+
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python3'},
+#     {'id': 2, 'name': 'Lets learn python2'},
+#     {'id': 3, 'name': 'Lets learn python1'},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'base/room_form.html', {"form": form})
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'base/room_form.html', {"form": form})
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+    return render(request, 'base/delete.html', {'obj': room})
